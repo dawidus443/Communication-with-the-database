6b02256604d78a8c79d69cdc184698ed79ac9ccc
Dawid Badora <dawidbadora@gmail.com>
2022-09-13 17:04:59 +0200

    Creating Data Access Layer

--- demo/src/main/java/com/example/demo/student/StudentService.java
+++ demo/src/main/java/com/example/demo/student/StudentService.java
@@ -1,22 +1,21 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 @Service
 public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents(){
-        return List.of(
-                new Student(
-                        1L,
-                        "Jackkk",
-                        "jack234@gmail.com",
-                        LocalDate.of(1998, Month.APRIL,5),
-                        23
-                )
-        );
+        return studentRepository.findAll();
     }
 }
